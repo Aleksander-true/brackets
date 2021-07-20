ea062a57c5ccc20728993cba7a006c910b7d086a
Aleksander-true <al_seryapin@mail.ru>
2021-07-20 14:45:38 +0300

    init: task brackets JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let openBracket = bracketsConfig.map((item)=> item[0]);
+  let closeBracket = bracketsConfig.map((item)=> item[1]);
+  let stackBrackets = [];
+  let isCorrect = true;
+
+  for (let index=0; index<=str.length; index++ ) {
+    if (openBracket.includes(str[index])) { 
+      if (closeBracket.includes(str[index])) {  
+        if (closeBracket.indexOf(str[index]) === stackBrackets[stackBrackets.length-1]) {
+          stackBrackets.pop() 
+        } else {stackBrackets.push(openBracket.indexOf(str[index]))};
+      } else {stackBrackets.push(openBracket.indexOf(str[index]))};
+      continue;
+      }
+    if (closeBracket.includes(str[index])) {
+      if (closeBracket.indexOf(str[index]) != stackBrackets.pop()) {isCorrect=false ; break;}
+    }
+  }
+  return (isCorrect && stackBrackets.length == 0); 
 }
